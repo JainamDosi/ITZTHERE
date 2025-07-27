5ed1d2a707a0517f500396cce4959ff9b99b5dba
Jainam <127018175+JainamDosi@users.noreply.github.com>
2025-07-27 22:42:10 +0530

    Update authController.js

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -226,7 +226,7 @@ export const verifyLoginOtp = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // true in production
-      sameSite: "lax", // or 'strict'
+      sameSite: "none", // or 'strict'
       // Adjust this if needed
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
